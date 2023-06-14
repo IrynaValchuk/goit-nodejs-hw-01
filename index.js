b93d9ebd88e33ce12c7f751971c37891f5322892
Iryna <irula17ooo@gmail.com>
2023-06-15 00:25:15 +0300

    add step 1-3

---
+++ index.js
@@ -0,0 +1,6 @@
+const contacts = require("./contacts");
+
+// contacts.listContacts();
+// contacts.getContactById("C9sjBfCo4UJCWjzBnOtxl");
+// contacts.removeContact("y3-LHhgxuyhTQFrG4IPml");
+// contacts.addContact("mano", "mano@gmail.com", "23-47-66");
